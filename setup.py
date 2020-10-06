fbe999be6ae6ff904d20084aed2ac37e251c5abc
ninjinkun <ninjin@mac.com>
2020-10-06 18:23:53 +0900

    feature: bump up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'launchable',
-    version = '0.0.1',
+    version = '0.0.2',
     license = 'MIT',
     author = 'Satoshi Asano',
     url = 'https://launchableinc.com/',
