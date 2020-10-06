b6de82d3c42b2772de00de60d2bd388676e3edd6
ninjinkun <ninjin@mac.com>
2020-10-06 18:21:17 +0900

    fix: README.md opener

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name = 'launchable',
