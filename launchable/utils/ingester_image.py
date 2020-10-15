daadff1177421d09d7dd1a1615ddcaab834a8b06
ninjinkun <ninjin@mac.com>
2020-10-15 17:21:07 +0900

    fix: lint with autopep8

--- launchable/utils/ingester_image.py
+++ launchable/utils/ingester_image.py
@@ -3,6 +3,6 @@
 # You can see the docker image tags at
 # https://hub.docker.com/repository/registry-1.docker.io/launchableinc/ingester/tags
 ingester_image = (
-  'launchableinc/ingester:'
-  '976171f4005f8a1c6d0e706d20760890b44c5373'
+    'launchableinc/ingester:'
+    '976171f4005f8a1c6d0e706d20760890b44c5373'
 )
