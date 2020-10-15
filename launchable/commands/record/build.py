d166b28dca5c02ff72768276f9f22f7f92329fb1
ninjinkun <ninjin@mac.com>
2020-10-15 17:28:58 +0900

    fix: lint

--- launchable/commands/record/build.py
+++ launchable/commands/record/build.py
@@ -8,8 +8,22 @@ from .commit import commit
 
 
 @click.command()
-@click.option('--name', 'build_number', help='build identifer', required=True, type=str, metavar='BUILD_ID')
-@click.option('--source', help='repository name and its commit hash. please specify repoName=hash pair like --source main=./main --source lib=./main/lib', default=["main=."], metavar="REPO_NAME", multiple=True)
+@click.option(
+    '--name',
+    'build_number',
+    help='build identifer',
+    required=True,
+    type=str,
+    metavar='BUILD_ID'
+)
+@click.option(
+    '--source',
+    help='repository name and its commit hash. please specify \
+    repoName=hash pair like --source main=./main --source lib=./main/lib',
+    default=["main=."],
+    metavar="REPO_NAME",
+    multiple=True
+)
 @click.option('--with-commit/--without-commit', help='', default=True)
 @click.pass_context
 def build(ctx, build_number, source, with_commit):
@@ -24,24 +38,33 @@ def build(ctx, build_number, source, with_commit):
         for (name, repo_dist) in repos:
             ctx.invoke(commit, source=repo_dist)
 
-    sources = [(name, subprocess.check_output("git rev-parse HEAD".split(),
-                                              cwd=repo_dist).decode()) for name, repo_dist in repos]
+    sources = [(
+        name,
+        subprocess.check_output(
+            "git rev-parse HEAD".split(), cwd=repo_dist
+        ).decode()
+    ) for name, repo_dist in repos]
     submodules = []
     for repo_name, repo_dist in repos:
         # invoke git directly because dulwich's submodule feature was broken
         submodule_stdouts = subprocess.check_output(
-            "git submodule status --recursive".split(), cwd=repo_dist).decode().splitlines()
+            "git submodule status --recursive".split(), cwd=repo_dist
+        ).decode().splitlines()
         for submodule_stdout in submodule_stdouts:
-            # the output is e.g. "+bbf213437a65e82dd6dda4391ecc5d598200a6ce sub1 (heads/master)"
+            # the output is e.g.
+            # "+bbf213437a65e82dd6dda4391ecc5d598200a6ce sub1 (heads/master)"
             matched = re.search(
-                r"^[\+\-U ](?P<hash>[a-f0-9]{40}) (?P<name>\w+)", submodule_stdout)
+                r"^[\+\-U ](?P<hash>[a-f0-9]{40}) (?P<name>\w+)",
+                submodule_stdout
+            )
             if matched:
                 hash = matched.group('hash')
                 name = matched.group('name')
                 if hash and name:
                     submodules.append((repo_name+"/"+name, hash))
 
-    # Note: currently becomes unique command args and submodules by the hash. But they can be conflict between repositories.
+    # Note: currently becomes unique command args and submodules by the hash.
+    # But they can be conflict between repositories.
     uniq_submodules = {hash: (name, hash)
                        for name, hash in sources + submodules}.values()
 
@@ -51,7 +74,8 @@ def build(ctx, build_number, source, with_commit):
             'commitHash': hash
         } for name, hash in uniq_submodules]
 
-        if not (commitHashes[0]['repositoryName'] and commitHashes[0]['commitHash']):
+        if not (commitHashes[0]['repositoryName']
+                and commitHashes[0]['commitHash']):
             exit('Please specify --source as --source .')
 
         payload = {
@@ -64,8 +88,8 @@ def build(ctx, build_number, source, with_commit):
             'Authorization': 'Bearer {}'.format(token)
         }
 
-        url = "https://api.mercury.launchableinc.com/intake/organizations/{}/workspaces/{}/builds".format(
-            org, workspace)
+        url = "https://api.mercury.launchableinc.com/intake/\
+          organizations/{}/workspaces/{}/builds".format(org, workspace)
 
         request = urllib.request.Request(
             url, data=json.dumps(payload).encode(), headers=headers)
