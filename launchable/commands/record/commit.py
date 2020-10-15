e0fa9a456886a0e03f5056e6b7015eaab9d3174c
ninjinkun <ninjin@mac.com>
2020-10-15 17:20:53 +0900

    fix: lint

--- launchable/commands/record/commit.py
+++ launchable/commands/record/commit.py
@@ -5,9 +5,16 @@ from ...utils.ingester_image import ingester_image
 
 
 @click.command()
-@click.option('--source', help="repository path", default=os.getcwd(), type=str)
+@click.option('--source',
+              help="repository path",
+              default=os.getcwd(),
+              type=str
+              )
 def commit(source):
     token, org, workspace = parse_token()
     source = os.path.abspath(source)
-    os.system("docker run -u $(id -u) -i --rm -v {}:{} --env LAUNCHABLE_TOKEN {} ingest:commit {}".format(
-        source, source, ingester_image, source))
+    os.system(
+        "docker run -u $(id -u) -i --rm \
+        -v {}:{} --env LAUNCHABLE_TOKEN {} ingest:commit {}"
+        .format(source, source, ingester_image, source)
+    )
