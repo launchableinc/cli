36840a42c9d9b55bbc4bec15828aac106cb0771b
ninjinkun <ninjin@mac.com>
2020-11-02 18:08:07 +0900

    feature: execute with jar

--- launchable/commands/record/commit.py
+++ launchable/commands/record/commit.py
@@ -2,6 +2,7 @@ from ...utils.token import parse_token
 import os
 import click
 from ...utils.ingester_image import ingester_image
+import subprocess
 
 
 @click.command()
@@ -9,12 +10,40 @@ from ...utils.ingester_image import ingester_image
               help="repository path",
               default=os.getcwd(),
               type=str
-              )
-def commit(source):
-    token, org, workspace = parse_token()
+)
+@click.option('--executable',
+              help="",
+              type=click.Choice(['jar', 'docker']),
+              default='jar'
+)
+def commit(source, executable):
     source = os.path.abspath(source)
+
+    if executable == 'jar':
+        exec_jar(source)
+    elif executable == 'docker':
+        exec_docker(source)
+
+
+def exec_jar(source):
+    res = subprocess.run(["which", "java"], stdout=subprocess.DEVNULL)
+    java = None
+    if res.returncode == 0:
+        java = "java"
+    elif os.access(os.path.expandvars("$JAVA_HOME/bin/java"), os.X_OK):
+        java = "$JAVA_HOME/bin/java"
+
+    if not java:
+        exit("You need to install Java or try --executable docker")
+
     os.system(
-        "docker run -u $(id -u) -i --rm \
-        -v {}:{} --env LAUNCHABLE_TOKEN {} ingest:commit {}"
+        "{} -jar bin/exe_deploy.jar ingest:commit {}"
+        .format(java, source))
+
+
+def exec_docker(source):
+    os.system(
+        "docker run -u $(id -u) -i --rm " \
+        "-v {}:{} --env LAUNCHABLE_TOKEN {} ingest:commit {}"
         .format(source, source, ingester_image, source)
     )
