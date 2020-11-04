bc76b20dd1f7549b150734ca9ad56cd014733050
ninjinkun <ninjin@mac.com>
2020-11-04 17:23:40 +0900

    feature: download exe_deploy.jar from S3

--- launchable/commands/record/commit.py
+++ launchable/commands/record/commit.py
@@ -2,7 +2,15 @@ from ...utils.token import parse_token
 import os
 import click
 from ...utils.ingester_image import ingester_image
+from ...utils.ingester_jar import ingester_jar_url, ingester_jar_version
 import subprocess
+import requests
+import pathlib
+import sys
+
+
+jar_dir_path = os.path.expanduser("~/.launchable/jar/ingester/{}".format(ingester_jar_version))
+jar_file_path = jar_dir_path + "/exe_deploy.jar"
 
 
 @click.command()
@@ -20,9 +28,37 @@ def commit(source, executable):
     source = os.path.abspath(source)
 
     if executable == 'jar':
+        download_jar()
         exec_jar(source)
     elif executable == 'docker':
         exec_docker(source)
+
+
+def download_jar():
+    pathlib.Path(jar_dir_path).mkdir(parents=True, exist_ok=True)
+
+    res = requests.get(ingester_jar_url, stream=True)
+    total_length = int(res.headers.get('content-length'))
+
+    if os.path.exists(jar_file_path) and os.path.getsize(jar_file_path) == total_length:
+        print("exe_deploy.jar exits at {}".format(jar_dir_path))
+    else:
+        total_downloaded = 0
+        f = open(file_path, 'wb')
+        for chunk in res.iter_content(chunk_size=512 * 1024):
+            if chunk:
+                f.write(chunk)
+
+                total_downloaded += len(chunk)
+                sys.stdout.write("\rDownloading exe_deploy.jar: {:.1f}% ({:,} / {:,} byte)".format(
+                    (total_downloaded / total_length) * 100),
+                    total_downloaded,
+                    total_length
+                )
+                sys.stdout.flush()
+
+        f.close()
+        print("\nexe_deploy.jar is downloaded at {}", jar_dir_path)
 
 
 def exec_jar(source):
@@ -37,8 +73,8 @@ def exec_jar(source):
         exit("You need to install Java or try --executable docker")
 
     os.system(
-        "{} -jar bin/exe_deploy.jar ingest:commit {}"
-        .format(java, source))
+        "{} -jar {} ingest:commit {}"
+        .format(java, jar_file_path, source))
 
 
 def exec_docker(source):
