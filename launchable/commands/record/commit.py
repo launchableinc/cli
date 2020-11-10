90534f8d23c24640421aa1eea33df9b613ce5fef
ninjinkun <ninjin@mac.com>
2020-11-10 23:26:25 +0900

    feature: invoke local jar directly

--- launchable/commands/record/commit.py
+++ launchable/commands/record/commit.py
@@ -1,19 +1,14 @@
 import os
 import click
 from ...utils.ingester_image import ingester_image
-from ...utils.ingester_jar import ingester_jar_url, ingester_jar_version
 import subprocess
 import requests
 import pathlib
 import sys
 import shutil
 
-tmp_dir_path = os.path.expanduser(
-    "/tmp/launchable/jar/ingester/{}".format(ingester_jar_version))
-tmp_file_path = tmp_dir_path + "/exe_deploy.jar"
-jar_dir_path = os.path.expanduser(
-    "~/.cache/launchable/jar/ingester/{}".format(ingester_jar_version))
-jar_file_path = jar_dir_path + "/exe_deploy.jar"
+jar_file_path = os.path.normpath(os.path.join(
+    os.path.dirname(__file__), "../../../bin/exe_deploy.jar"))
 
 
 @click.command()
@@ -32,46 +27,11 @@ def commit(source, executable):
 
     if executable == 'jar':
         try:
-            download_jar()
             exec_jar(source)
         except Exception as e:
             print(e)
     elif executable == 'docker':
         exec_docker(source)
-
-
-def download_jar():
-    if os.path.exists(jar_file_path):
-        print("exe_deploy.jar exists in {}".format(jar_dir_path))
-    else:
-        res = requests.get(ingester_jar_url, stream=True)
-        res.raise_for_status()
-
-        total_length = int(res.headers.get('content-length'))
-        total_downloaded = 0
-
-        os.makedirs(tmp_dir_path, exist_ok=True)
-        f = open(tmp_file_path, 'wb')
-
-        print("Downloading exe_deploy.jar ({:,.2f} MB)".format(
-            total_length / (1024 * 1024)))
-
-        for chunk in res.iter_content(chunk_size=1024 * 1024 * 10):
-            if chunk:
-                f.write(chunk)
-                total_downloaded += len(chunk)
-
-                sys.stdout.write("\r{}".format(
-                    "." * int(total_downloaded / (1024 * 1024 * 10))
-                ))
-                sys.stdout.flush()
-
-        f.close()
-
-        os.makedirs(jar_dir_path, exist_ok=True)
-        shutil.move(tmp_file_path, jar_dir_path)
-        sys.stdout.write(
-            "\nexe_deploy.jar is downloaded in {}\n".format(jar_dir_path))
 
 
 def exec_jar(source):
