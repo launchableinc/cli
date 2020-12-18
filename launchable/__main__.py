19b41c74a1c9e3b48ac4d652784522d495dbae4f
Kohsuke Kawaguchi <kkawaguchi@launchableinc.com>
2020-12-18 15:11:24 -0800

    minor touch up

--- launchable/__main__.py
+++ launchable/__main__.py
@@ -2,7 +2,7 @@ from .version import __version__
 import click
 import importlib
 from os.path import dirname, basename, join
-import glob
+from glob import glob
 from .commands.record import record
 from .commands.optimize import optimize
 from .commands.verify import verify
@@ -11,7 +11,7 @@ from .commands.verify import verify
 @click.version_option(version=__version__, prog_name='launchable-cli')
 def main():
     # load all test runners
-    for f in glob.glob(join(dirname(__file__), 'test_runners', "*.py")):
+    for f in glob(join(dirname(__file__), 'test_runners', "*.py")):
         f = basename(f)[:-3]
         if f == '__init__':
             continue
