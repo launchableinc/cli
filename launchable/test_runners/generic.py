03d98c147265381aa0971cfa429ad47fe8c93a4a
Kohsuke Kawaguchi <kkawaguchi@launchableinc.com>
2020-12-21 15:29:51 -0800

    Test path alignment for the generic parser support

--- launchable/test_runners/generic.py
+++ launchable/test_runners/generic.py
@@ -8,6 +8,7 @@ from . import launchable
 
 @launchable.optimize.tests
 def optimize_tests(client):
+    # read lines as test file names
     for t in sys.stdin:
         client.test_path(t)
     client.run()
@@ -16,6 +17,9 @@ def optimize_tests(client):
 @click.argument('reports', required=True, nargs=-1)
 @launchable.record.tests
 def record_tests(client, reports):
+    # here we are implicitly assuming that test reports we read, combined with the default path builder,
+    # produces test names that aligned with what `optimize_tests` function does above.
+    # TODO: use a custom path builder to ensure this is always the case. This is too fragile.
     for r in reports:
         client.report(r)
     client.run()
