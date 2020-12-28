4bf7746f0ed9862e751fdd7438a04ab7516825fe
ninjinkun <ninjin@mac.com>
2020-12-28 21:23:12 +0900

    feature: add --gtest_list_tests support

--- launchable/test_runners/googletest.py
+++ launchable/test_runners/googletest.py
@@ -13,12 +13,25 @@ def make_test_path(cls, case) -> TestPath:
 @launchable.subset
 def subset(client):
     # Read targets from stdin, which generally looks like //foo/bar:zot
-    for label in sys.stdin:
+    cls = ''
+    for label in map(str.rstrip, sys.stdin):
+        # handle Google Test's --gtest_list_tests output
+        # FooTest.
+        #  Bar
+        #  Baz
+        gtest_class = re.match(r'^([^\.]+)\.', label)
+        if gtest_class:
+            cls = gtest_class.group(1)
+        gtest_case = re.match(r'^  ([^ ]+)', label)
+        if gtest_case and cls:
+            case = gtest_case.group(1)
+            client.test_path(make_test_path(cls, case))
+        
         # handle ctest -N output
         # Test #1: FooTest.Bar -> (FooTest, Bar)
-        result = re.match(r'^ *Test #\d+: ([^ ]+)$', label)
-        if result:
-            (cls, case) = result.group(1).split('.')
+        ctest_result = re.match(r'^  Test #\d+: ([^ ]+)$', label)
+        if ctest_result:
+            (cls, case) = ctest_result.group(1).split('.')
             client.test_path(make_test_path(cls, case))
 
     client.formatter = lambda x: x[0]['name'] + "." + x[1]['name']
