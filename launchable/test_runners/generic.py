1be06d82efa825931f42f34216721259b302d87d
Kohsuke Kawaguchi <kkawaguchi@launchableinc.com>
2021-01-06 18:45:40 -0800

    [ST-657] shippable generic test runner support
    Made more robust by implementing a dedicated test path builder with
    error checks.

--- launchable/test_runners/generic.py
+++ launchable/test_runners/generic.py
@@ -4,6 +4,8 @@
 import click
 import sys
 from . import launchable
+from junitparser import TestCase, TestSuite
+from ..testpath import TestPath
 
 
 @launchable.subset
@@ -17,9 +19,31 @@ def subset(client):
 @click.argument('reports', required=True, nargs=-1)
 @launchable.record.tests
 def record_tests(client, reports):
-    # here we are implicitly assuming that test reports we read, combined with the default path builder,
-    # produces test names that aligned with what `optimize_tests` function does above.
-    # TODO: use a custom path builder to ensure this is always the case. This is too fragile.
+    def path_builder(case: TestCase, suite: TestSuite, report_file: str) -> TestPath:
+        """path builder that puts the file name first, which is consistent with the subset command"""
+        def find_filename():
+            """look for what looks like file names from test reports"""
+            for e in [case, suite]:
+                for a in ["file","filepath"]:
+                    filepath = e._elem.attrib.get(a)
+                    if filepath:
+                        return filepath
+            return None # failing to find a test name
+
+
+        filepath = find_filename()
+        if not filepath:
+            raise click.ClickException("No file name found in %s" % report_file)
+
+        # default test path in `subset` expects to have this file name
+        test_path = [client.make_file_path_component(filepath)]
+        if suite.name:
+            test_path.append({"type": "testsuite", "name": suite.name})
+        if case.name:
+            test_path.append({"type": "testcase", "name": case.name})
+        return test_path
+    client.path_builder = path_builder
+
     for r in reports:
         client.report(r)
     client.run()
