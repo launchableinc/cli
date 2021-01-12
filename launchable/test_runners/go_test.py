0d7743e18a85309d0dae810a3f38113b65b07f6b
awilkes <awilkes@users.noreply.github.com>
2021-01-12 15:52:19 -0700

    Update launchable/test_runners/go_test.py

--- launchable/test_runners/go_test.py
+++ launchable/test_runners/go_test.py
@@ -6,7 +6,7 @@ from . import launchable
 @launchable.subset
 def subset(client):
     for case in sys.stdin:
-        # Aboid last line such s `ok      github.com/launchableinc/rocket-car-gotest      0.268s`
+        # Avoid last line such as `ok      github.com/launchableinc/rocket-car-gotest      0.268s`
         if not ' ' in case:
             client.test_path({'type': 'testcase', 'name': case.rstrip('\n')})
     client.run()
