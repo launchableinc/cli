122f1237da2ee6027f541bec8d5165b2193f4980
Ryosuke Yabuki <ryabuki@launchableinc.com>
2021-01-19 16:27:08 +0900

    format use by autopep8

--- tests/test_runners/test_googletest.py
+++ tests/test_runners/test_googletest.py
@@ -5,12 +5,14 @@ from tests.cli_test_case import CliTestCase
 
 
 class GoogleTestTest(CliTestCase):
-    test_files_dir = Path(__file__).parent.joinpath('../data/googletest/').resolve()
+    test_files_dir = Path(__file__).parent.joinpath(
+        '../data/googletest/').resolve()
     result_file_path = test_files_dir.joinpath('record_test_result.json')
 
     @mock.patch('requests.request')
     def test_record_test_gradle(self, mock_post):
-        result = self.cli('record', 'tests',  '--session', self.session, 'googletest', str(self.test_files_dir) + "/")
+        result = self.cli('record', 'tests',  '--session', self.session,
+                          'googletest', str(self.test_files_dir) + "/")
         self.assertEqual(result.exit_code, 0)
 
         payload = self.gzipped_json_payload(mock_post)
