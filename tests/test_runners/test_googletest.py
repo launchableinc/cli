e0beda40896c3f409b72a8100073f06f00ee74f2
Ryosuke Yabuki <ryabuki@launchableinc.com>
2021-01-20 12:19:41 +0900

    add test for googletest's subset test

--- tests/test_runners/test_googletest.py
+++ tests/test_runners/test_googletest.py
@@ -7,7 +7,18 @@ from tests.cli_test_case import CliTestCase
 class GoogleTestTest(CliTestCase):
     test_files_dir = Path(__file__).parent.joinpath(
         '../data/googletest/').resolve()
-    result_file_path = test_files_dir.joinpath('record_test_result.json')
+
+    @mock.patch('requests.request')
+    def test_subset(self, mock_post):
+        pipe = "Test project github.com/launchableinc/rocket-car-googletest\n  Test #1: FooTest.Bar\n  Test #2: FooTest.Baz\n  Test #3: FooTest.Foo\n  Test #4: */ParameterizedTest.Bar/*\n\nTotal Tests: 4"
+        result = self.cli('subset', '--target', '10%',
+                          '--session', self.session, 'googletest', input=pipe)
+        self.assertEqual(result.exit_code, 0)
+
+        payload = self.gzipped_json_payload(mock_post)
+        expected = self.load_json_from_file(
+            self.test_files_dir.joinpath('subset_result.json'))
+        self.assert_json_orderless_equal(expected, payload)
 
     @mock.patch('requests.request')
     def test_record_test_googletest(self, mock_post):
@@ -16,6 +27,7 @@ class GoogleTestTest(CliTestCase):
         self.assertEqual(result.exit_code, 0)
 
         payload = self.gzipped_json_payload(mock_post)
-        expected = self.load_json_from_file(self.result_file_path)
+        expected = self.load_json_from_file(
+            self.test_files_dir.joinpath('record_test_result.json'))
 
         self.assert_json_orderless_equal(expected, payload)
