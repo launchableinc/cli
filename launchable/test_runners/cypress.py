242d9d64bc92b4898392f2c8cb6418f185d7f0cb
Ryosuke Yabuki <ryabuki@launchableinc.com>
2021-01-21 15:55:04 +0900

    fix to support cypress format

--- launchable/test_runners/cypress.py
+++ launchable/test_runners/cypress.py
@@ -1,6 +1,7 @@
 import click
 import os
 from . import launchable
+from junitparser import TestSuite, JUnitXml
 
 
 @click.argument('reports', required=True, nargs=-1)
@@ -9,4 +10,21 @@ def record_tests(client, reports):
     for r in reports:
         client.report(r)
 
+    def testsuites(xml) -> [TestSuite]:
+        testsuites = []
+        if isinstance(xml, JUnitXml):
+            for suite in xml:
+                if suite.name == "Root Suite":
+                    filepath = suite._elem.attrib.get("file")
+                    continue
+                if filepath:
+                    suite._elem.attrib.update(
+                        {"filepath": filepath})
+                    testsuites.append(suite)
+        else:
+            # TODO: what is a Pythonesque way to do this?
+            assert False
+        return testsuites
+
+    client.testsuites = testsuites
     client.run()
