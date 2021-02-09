62efb7649d7f3bdacee6e3099c809db4dedddbdb
Ryosuke Yabuki <ryabuki@launchableinc.com>
2021-02-09 17:17:13 +0900

    add implementation for close session

--- launchable/commands/close/session.py
+++ launchable/commands/close/session.py
@@ -1,4 +1,9 @@
+import os
+import traceback
+
 import click
+from ...utils.http_client import LaunchableClient
+from ...utils.token import parse_token
 
 
 @click.command()
@@ -7,6 +12,22 @@ import click
     'session_id',
     help='Test session ID',
     type=str,
+    required=True,
 )
 def session(session_id: str):
-    print(session_id)
+    token, org, workspace = parse_token()
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    client = LaunchableClient(token)
+    try:
+        res = client.request(
+            "patch", "{}/close".format(session_id), headers=headers)
+        res.raise_for_status()
+    except Exception as e:
+        if os.getenv(REPORT_ERROR_KEY):
+            raise e
+        else:
+            traceback.print_exc()
