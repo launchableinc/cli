e4aebba4ee9920fe7a59fe9e4625e6c1b65929b8
ninjinkun <ninjin@mac.com>
2021-02-17 18:48:07 +0900

    feature: add proxy support in `record commit` command

---
+++ tests/commands/record/test_commit.py
@@ -0,0 +1,9 @@
+import unittest
+from launchable.commands.record.commit import _build_proxy_option
+
+class CommitTest(unittest.TestCase):
+  def test_proxy_options(self):
+      self.assertEqual(_build_proxy_option("https://some_proxy:1234"), "-Dhttps.proxyHost=some_proxy -Dhttps.proxyPort=1234 ")
+      self.assertEqual(_build_proxy_option("some_proxy:1234"), "-Dhttps.proxyHost=some_proxy -Dhttps.proxyPort=1234 ")
+      self.assertEqual(_build_proxy_option("some_proxy"), "-Dhttps.proxyHost=some_proxy ")
+      self.assertEqual(_build_proxy_option("https://some_proxy"), "-Dhttps.proxyHost=some_proxy ")
