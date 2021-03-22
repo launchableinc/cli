805cae1c9b1c4ebf7239fb07cf7bf8b445683b7d
Yoshiori SHOJI <yshoji@launchableinc.com>
2021-03-22 14:45:45 +0900

    add rspec plugin

---
+++ launchable/test_runners/rspec.py
@@ -0,0 +1,35 @@
+import click
+import os
+import sys
+from . import launchable
+
+
+@click.argument('files', required=True, nargs=-1)
+@launchable.subset
+def subset(client, files):
+    def parse(fname: str):
+        '''
+        Scan a file, directory full of *.rb, or @FILE
+        '''
+        if os.path.isdir(fname):
+            client.scan(fname, '**/*_spec.rb')
+        elif fname == '@-':
+            # read stdin
+            for l in sys.stdin:
+                parse(l)
+        elif fname.startswith('@'):
+            # read response file
+            with open(fname[1:]) as f:
+                for l in f:
+                    parse(l)
+        else:
+            # assume it's a file
+            client.test_path(fname)
+
+    for f in files:
+        parse(f)
+
+    client.run()
+
+
+record_tests = launchable.CommonRecordTestImpls(__name__).report_files()
