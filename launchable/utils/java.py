d14cc94e70cbf5df23a05f6a4bf629ee4843a02d
ninjinkun <ninjin@mac.com>
2021-04-14 12:58:55 +0900

    fix: use shutil.which for supporting Windows

--- launchable/utils/java.py
+++ launchable/utils/java.py
@@ -1,10 +1,8 @@
 import os
-import subprocess
-
+import shutil
 
 def get_java_command():
-    res = subprocess.run(["which", "java"], stdout=subprocess.DEVNULL)
-    if res.returncode == 0:
+    if shutil.which("java"):
         return "java"
 
     if os.access(os.path.expandvars("$JAVA_HOME/bin/java"), os.X_OK):
