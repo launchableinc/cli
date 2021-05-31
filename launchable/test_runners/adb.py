64cb2147806245ae94b2e797bd8d596b06a0a500
ninjinkun <ninjin@mac.com>
2021-05-31 20:35:34 +0900

    feature: add ADB plugin

---
+++ launchable/test_runners/adb.py
@@ -0,0 +1,22 @@
+from . import launchable
+import re
+
+
+@launchable.subset
+def subset(client):
+    prev_cls_name = None
+    for line in client.stdin():
+        match = re.match(r'^INSTRUMENTATION_STATUS: class=(.+)$', line)
+        if match:
+            cls_name = match.group(1)
+            if prev_cls_name != cls_name:
+                client.test_path([{"type": "class", "name": cls_name}])
+                prev_cls_name = cls_name
+    
+    client.separator = ','
+
+    client.run()
+
+
+record_tests = launchable.CommonRecordTestImpls(__name__).report_files()
+    
