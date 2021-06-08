6e016c3235832f98fcee85b71334aef44d6135f1
shuheiktgw <shuheiktgw@users.noreply.github.com>
2021-06-08 18:08:54 +0900

    Set GitHub-Pr-Head-Sha header only when it exists

--- launchable/utils/authentication.py
+++ launchable/utils/authentication.py
@@ -21,15 +21,20 @@ def authentication_headers():
         return {'Authorization': 'Bearer {}'.format(token)}
 
     if os.getenv('GITHUB_ACTIONS'):
-        return {
+        headers = {
             'GitHub-Actions': os.environ['GITHUB_ACTIONS'],
             'GitHub-Run-Id': os.environ['GITHUB_RUN_ID'],
             'GitHub-Repository': os.environ['GITHUB_REPOSITORY'],
             'GitHub-Workflow': os.environ['GITHUB_WORKFLOW'],
             'GitHub-Run-Number': os.environ['GITHUB_RUN_NUMBER'],
             'GitHub-Event-Name': os.environ['GITHUB_EVENT_NAME'],
-            # GITHUB_PR_HEAD_SHA might not exist
-            'GitHub-Pr-Head-Sha': os.getenv('GITHUB_PR_HEAD_SHA'),
             'GitHub-Sha': os.environ['GITHUB_SHA'],
         }
+
+        # GITHUB_PR_HEAD_SHA might not exist
+        pr_head_sha = os.getenv('GITHUB_PR_HEAD_SHA')
+        if pr_head_sha:
+            headers['GitHub-Pr-Head-Sha'] = pr_head_sha
+
+        return headers
     return {}
