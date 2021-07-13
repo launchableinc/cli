9d55ce33d5f2c01c55985114eafc70111061e34e
ninjinkun <ninjin@mac.com>
2021-07-13 18:38:08 +0900

    fix: print debug

--- launchable/commands/subset.py
+++ launchable/commands/subset.py
@@ -239,6 +239,8 @@ def subset(context, target, session: Optional[str], base_path: Optional[str], bu
             else:
                 # regardless of whether we managed to talk to the service
                 # we produce test names
+                print(os.name)
+                print(os.linesep)
                 if rest:
                     if len(rests) == 0:
                         rests.append(output[0])
