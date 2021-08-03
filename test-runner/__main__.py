1c101b305f459f0124c008d0a6e7d73742403a25
Ryosuke Yabuki <ryabuki@launchableinc.com>
2021-08-03 16:37:28 +0900

    add comment

--- test-runner/__main__.py
+++ test-runner/__main__.py
@@ -1,6 +1,13 @@
 import sys
 from xmlrunner.runner import XMLTestProgram, XMLTestRunner
 
+"""
+This project use xmlrunner command to publish xml test report.
+However, the xmlrunner adds a date time suffix to test suite automatically.
+Although, there are the same test cases, the Launchable realizes the differect test cases.
+So I add this wrapper command to disable suffix.
+"""
+
 
 class LaunchableTestRunner(XMLTestRunner):
     def __init__(self, **kwargs):
