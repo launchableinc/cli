ce677e62d29ba7f4c5ec30fee051450739f15af4
Masaya Suzuki <draftcode@gmail.com>
2021-08-18 16:53:57 -0700

    Disable some tests for Windows

--- tests/test_testpath.py
+++ tests/test_testpath.py
@@ -1,10 +1,11 @@
 from launchable.testpath import FilePathNormalizer
 
+import os.path
+import pathlib
 import subprocess
+import sys
 import tempfile
 import unittest
-import pathlib
-import os.path
 
 
 class TestFilePathNormalizer(unittest.TestCase):
@@ -42,6 +43,9 @@ class TestFilePathNormalizer(unittest.TestCase):
         self.assertEqual(relpath, n.relativize(relpath))
         self.assertEqual(relpath, n.relativize(abspath))
 
+    @unittest.skipIf(
+            sys.platform.startswith("win"),
+            "tempfile creates 8.3 filenames, and it's hard to deal with them. Practically, we don't see them often, so do not support them now until it's needed.")
     def test_inference_git(self):
         with tempfile.TemporaryDirectory() as tempdirname:
             temppath = pathlib.PurePath(tempdirname)
@@ -54,6 +58,9 @@ class TestFilePathNormalizer(unittest.TestCase):
             n = FilePathNormalizer()
             self.assertEqual(relpath, n.relativize(abspath))
 
+    @unittest.skipIf(
+            sys.platform.startswith("win"),
+            "tempfile creates 8.3 filenames, and it's hard to deal with them. Practically, we don't see them often, so do not support them now until it's needed. Also when this runs on Windows, GIT_AUTHOR_NAME etc. is ignored and fails.")
     def test_inference_git_submodule(self):
         with tempfile.TemporaryDirectory() as tempdirname:
             temppath = pathlib.PurePath(tempdirname)
