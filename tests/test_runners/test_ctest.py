fd2bb3b13eb3b664bd5cb7b67df0b03ab258a5d6
Masaya Suzuki <draftcode@gmail.com>
2021-09-24 13:28:27 -0700

    Add an option for ctest to write regex into split files
    CTest has a size limit in the regex engine
    (https://gitlab.kitware.com/cmake/cmake/-/blob/2974ea7c3e9cbf9952d6c361bbcecb5027f69605/Source/kwsys/RegularExpression.cxx#L362).
    Because of this, we cannot specify a large regex for test subsets.
    
    This change teaches the ctest profile to split the regex and output
    files in the specified directory. The caller can opt-in to this new
    behavior with --output-regex-files. With this, the CLI writes out the
    regex to multiple files, limiting each file size so that it fits to
    the CTest regex upperbound.

--- tests/test_runners/test_ctest.py
+++ tests/test_runners/test_ctest.py
@@ -3,7 +3,10 @@ import responses  # type: ignore
 import json
 import gzip
 import os
+import sys
+import tempfile
 from launchable.utils.session import read_session
+from launchable.utils.http_client import get_base_url
 from tests.cli_test_case import CliTestCase
 from unittest import mock
 
@@ -11,6 +14,65 @@ from unittest import mock
 class CTestTest(CliTestCase):
     test_files_dir = Path(__file__).parent.joinpath(
         '../data/ctest/').resolve()
+
+    @responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_subset_multiple_files(self):
+        responses.replace(
+            responses.POST,
+            "{}/intake/organizations/{}/workspaces/{}/subset".format(
+                get_base_url(), self.organization, self.workspace),
+            json={
+                "testPaths": [
+                    [{'type': 'testcase', 'name': 'FooTest.Bar'}],
+                    [{'type': 'testcase', 'name': 'FooTest.Foo'}],
+                    [{'type': 'testcase', 'name': '*/ParameterizedTest.Bar/*'}],
+                ],
+                "rest": [
+                    [{'name': 'FooTest.Baz'}],
+                ],
+                "subsettingId": 456,
+                "summary": {
+                    "subset": {"candidates": 4, "duration": 4, "rate": 90},
+                    "rest": {"candidate": 1, "duration": 0, "rate": 0}
+                },
+                "isBrainless": False
+            },
+            status=200)
+        with tempfile.TemporaryDirectory() as tempdir:
+            # Use a non-existing dir to check it creates a dir.
+            output_dir = os.path.join(tempdir, 'subdir')
+            result = self.cli('subset', '--target', '10%', '--build',
+                              self.build_name, 'ctest',
+                              '--output-regex-files',
+                              '--output-regex-file-dir=' + output_dir,
+                              '--output-regex-file-size=32',
+                              str(self.test_files_dir.joinpath("ctest_list.json")))
+            self.assertEqual(result.exit_code, 0)
+
+            subset_files = []
+            rest_files = []
+            for file in os.listdir(output_dir):
+                with open(os.path.join(output_dir, file), 'r') as f:
+                    if file.startswith('subset'):
+                        subset_files.append(f.read().strip())
+                    else:
+                        rest_files.append(f.read().strip())
+            subset_files.sort()
+            rest_files.sort()
+            if sys.version_info[:2] >= (3, 7):
+                self.assertEqual(subset_files, [
+                    '^FooTest\\.Bar$|^FooTest\\.Foo$',
+                    '^\\*/ParameterizedTest\\.Bar/\\*$'
+                ])
+            else:
+                # There was a change in re.escape behavior from Python 3.7. See
+                # https://docs.python.org/3/library/re.html#re.escape
+                self.assertEqual(subset_files, [
+                    '^FooTest\\.Bar$|^FooTest\\.Foo$',
+                    '^\\*\\/ParameterizedTest\\.Bar\\/\\*$'
+                ])
+            self.assertEqual(rest_files, ['^FooTest\\.Baz$'])
 
     @responses.activate
     @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
