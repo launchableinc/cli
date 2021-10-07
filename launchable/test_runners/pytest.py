cc5eda4a8d669adcaf6d966b0ee9afcebf112782
Yoshiori SHOJI <yshoji@launchableinc.com>
2021-10-07 20:58:24 +0900

    Implement backward compatibility for pytest

--- launchable/test_runners/pytest.py
+++ launchable/test_runners/pytest.py
@@ -1,10 +1,10 @@
 from typing import List
 from os.path import *
+import subprocess
 import click
 import os
 import glob
 from . import launchable
-from ..utils.file_name_pattern import pytest_test_pattern
 
 # Please specify junit_family=legacy for pytest report format. if using pytest version 6 or higher.
 # - pytest has changed its default test report format from xunit1 to xunit2 since version 6.
@@ -12,12 +12,10 @@ from ..utils.file_name_pattern import pytest_test_pattern
 # - The xunit2 format no longer includes file names.
 #   - It is possible to output in xunit1 format by specifying junit_family=legacy.
 #   - The xunit1 format includes the file name.
-
-
 @click.argument('source_roots', required=False, nargs=-1)
 @launchable.subset
 def subset(client, source_roots: List[str]):
-    if not source_roots:
+    def _add_testpaths(lines: List[str]):
         # for Example
         # $ pytest --collect-only -q
         # > tests/test_mod.py::TestClass::test__can_print_aaa
@@ -28,7 +26,7 @@ def subset(client, source_roots: List[str]):
         # result.xml(junit)
         # <testcase classname="tests.fooo.func4_test" name="test_func6" file="tests/fooo/func4_test.py" line="0" time="0.000" />
         # <testcase classname="tests.test_mod.TestClass" name="test__can_print_aaa" file="tests/test_mod.py" line="3" time="0.001" />
-        for label in client.stdin():
+        for label in lines:
             label = label.rstrip()
             # When an empty line comes, it's done.
             if not label:
@@ -46,18 +44,19 @@ def subset(client, source_roots: List[str]):
                 class_name += "." + data[1]
             test_path.append({"type": "class", "name": class_name})
             client.test_path(test_path)
+    if not source_roots:
+        _add_testpaths(client.stdin())
     else:
-        def add(f: str):
-            if pytest_test_pattern.match(basename(f)):
-                client.test_path(
-                    [{"type": "file", "name": os.path.normpath(f)}])
-        for root in source_roots:
-            for b in glob.iglob(root):
-                if isdir(b):
-                    for t in glob.iglob(join(b, '**/*.py'), recursive=True):
-                        add(t)
-                else:
-                    add(b)
+        command = ["pytest", "--collect-only", "-q"]
+        command.extend(source_roots)
+        try:
+            result = subprocess.run(
+                command, stdout=subprocess.PIPE, universal_newlines=True)
+            _add_testpaths(result.stdout.split(os.linesep))
+        except FileNotFoundError:
+            raise click.ClickException(
+                "pytest command not found. Please check the path.")
+
     client.formatter = lambda x: "{}".format(x)
     client.run()
 
