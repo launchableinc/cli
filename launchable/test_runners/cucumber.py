2ea5f305244456ed576a4ffa18a5427a318af198
Ryosuke Yabuki <ryabuki@launchableinc.com>
2022-01-26 15:58:45 +0900

    use TestPathNormalizer

--- launchable/test_runners/cucumber.py
+++ launchable/test_runners/cucumber.py
@@ -4,6 +4,8 @@ from typing import Dict, Generator, List, Optional
 from xml.etree import ElementTree as ET
 import click
 
+from launchable.testpath import FilePathNormalizer
+
 
 from ..commands.record.case_event import CaseEvent, CaseEventType
 from . import launchable
@@ -25,7 +27,7 @@ def record_tests(client, reports, json_format):
     if json_format:
         for r in reports:
             client.report(r)
-        client.parse_func = _record_tests_from_json
+        client.parse_func = JSONReportParser(client).parse_func
     else:
         report_file_and_test_file_map = {}
         _record_tests_from_xml(client, reports, report_file_and_test_file_map)
@@ -59,116 +61,127 @@ def _record_tests_from_xml(client, reports, report_file_and_test_file_map: Dict[
             click.echo("Cannot find test file of {}".format(report), err=True)
 
 
-def _record_tests_from_json(report_file: str) -> Generator[CaseEventType, None, None]:
+class JSONReportParser:
     """
-    example of JSON format report
-    [
-    {
-        "uri": "features/foo/is_it_friday_yet.feature",
-        "id": "is-it-friday-yet?",
-        "keyword": "Feature",
-        "name": "Is it Friday yet?",
-        "description": "  Everybody wants to know when it's Friday",
-        "line": 1,
-        "elements": [
+        client: launchable.RecordTests
+    """
+
+    def __init__(self, client):
+        self.client = client
+        self.file_path_normalizer = FilePathNormalizer(
+            base_path=client.base_path, no_base_path_inference=client.no_base_path_inference)
+
+    def parse_func(self, report_file: str) -> Generator[CaseEventType, None, None]:
+        """
+        example of JSON format report
+        [
         {
-            "id": "is-it-friday-yet?;today-is-or-is-not-friday;;2",
-            "keyword": "Scenario Outline",
-            "name": "Today is or is not Friday",
-            "description": "",
-            "line": 11,
-            "type": "scenario",
-            "steps": [
+            "uri": "features/foo/is_it_friday_yet.feature",
+            "id": "is-it-friday-yet?",
+            "keyword": "Feature",
+            "name": "Is it Friday yet?",
+            "description": "  Everybody wants to know when it's Friday",
+            "line": 1,
+            "elements": [
             {
-                "keyword": "Given ",
-                "name": "today is \"Friday\"",
-                "line": 5,
-                "match": {
-                "location": "features/step_definitions/stepdefs.rb:12"
+                "id": "is-it-friday-yet?;today-is-or-is-not-friday;;2",
+                "keyword": "Scenario Outline",
+                "name": "Today is or is not Friday",
+                "description": "",
+                "line": 11,
+                "type": "scenario",
+                "steps": [
+                {
+                    "keyword": "Given ",
+                    "name": "today is \"Friday\"",
+                    "line": 5,
+                    "match": {
+                    "location": "features/step_definitions/stepdefs.rb:12"
+                    },
+                    "result": {
+                    "status": "passed",
+                    "duration": 101000
+                    }
                 },
-                "result": {
-                "status": "passed",
-                "duration": 101000
-                }
-            },
-            {
-                "keyword": "When ",
-                "name": "I ask whether it's Friday yet",
-                "line": 6,
-                "match": {
-                "location": "features/step_definitions/stepdefs.rb:24"
+                {
+                    "keyword": "When ",
+                    "name": "I ask whether it's Friday yet",
+                    "line": 6,
+                    "match": {
+                    "location": "features/step_definitions/stepdefs.rb:24"
+                    },
+                    "result": {
+                    "status": "passed",
+                    "duration": 11000
+                    }
                 },
-                "result": {
-                "status": "passed",
-                "duration": 11000
+                {
+                    "keyword": "Then ",
+                    "name": "I should be told \"TGIF\"",
+                    "line": 7,
+                    "match": {
+                    "location": "features/step_definitions/stepdefs.rb:28"
+                    },
+                    "result": {
+                    "status": "passed",
+                    "duration": 481000
+                    }
                 }
-            },
-            {
-                "keyword": "Then ",
-                "name": "I should be told \"TGIF\"",
-                "line": 7,
-                "match": {
-                "location": "features/step_definitions/stepdefs.rb:28"
-                },
-                "result": {
-                "status": "passed",
-                "duration": 481000
-                }
+                ]
             }
-            ]
-        }
-    ]
-    """
-    with open(report_file, 'r') as json_file:
-        try:
-            data = json.load(json_file)
-        except Exception as e:
-            raise Exception("Can't read JSON format report file {}. Make sure to confirm report file.".format(
-                report_file)) from e
+        ]
+        """
+        with open(report_file, 'r') as json_file:
+            try:
+                data = json.load(json_file)
+            except Exception as e:
+                raise Exception("Can't read JSON format report file {}. Make sure to confirm report file.".format(
+                    report_file)) from e
 
-    if len(data) == 0:
-        click.echo("Can't find test reports from {}. Make sure to confirm report file.".format(
-            report_file), err=True)
+        if len(data) == 0:
+            click.echo("Can't find test reports from {}. Make sure to confirm report file.".format(
+                report_file), err=True)
 
-    for d in data:
-        file_name = d.get("uri", "")
-        class_name = d.get("name", "")
-        for element in d.get("elements", []):
-            test_case = element.get("name", "")
-            steps = {}
-            duration = 0
-            statuses = []
-            stderr = []
-            for step in element.get("steps", []):
-                steps[step.get("keyword", "").strip()] = step.get(
-                    "name", "").strip()
-                result = step.get("result", None)
+        for d in data:
+            file_name = d.get("uri", "")
+            class_name = d.get("name", "")
+            for element in d.get("elements", []):
+                test_case = element.get("name", "")
+                steps = {}
+                duration = 0
+                statuses = []
+                stderr = []
+                for step in element.get("steps", []):
+                    steps[step.get("keyword", "").strip()] = step.get(
+                        "name", "").strip()
+                    result = step.get("result", None)
 
-                if result:
-                    # duration's unit is nano sec
-                    # ref: https://github.com/cucumber/cucumber-ruby/blob/main/lib/cucumber/formatter/json.rb#L222
-                    duration = duration + result.get("duration", 0)
-                    statuses.append(result.get("status"))
-                    if result.get("error_message", None):
-                        stderr.append(result["error_message"])
+                    if result:
+                        # duration's unit is nano sec
+                        # ref: https://github.com/cucumber/cucumber-ruby/blob/main/lib/cucumber/formatter/json.rb#L222
+                        duration = duration + result.get("duration", 0)
+                        statuses.append(result.get("status"))
+                        if result.get("error_message", None):
+                            stderr.append(result["error_message"])
 
-            if "failed" in statuses:
-                status = CaseEvent.TEST_FAILED
-            elif "undefined" in statuses:
-                status = CaseEvent.TEST_SKIPPED
-            else:
-                status = CaseEvent.TEST_PASSED
+                if "failed" in statuses:
+                    status = CaseEvent.TEST_FAILED
+                elif "undefined" in statuses:
+                    status = CaseEvent.TEST_SKIPPED
+                else:
+                    status = CaseEvent.TEST_PASSED
 
-            test_path = [
-                {"type": "file", "name": file_name},
-                {"type": "class", "name": class_name},
-                {"type": "testcase", "name": test_case},
-            ]
+                test_path = [
+                    self.client.make_file_path_component(
+                        self.file_path_normalizer.relativize(file_name)),
+                    {"type": "class", "name": class_name},
+                    {"type": "testcase", "name": test_case},
+                ]
 
-            for k in steps:
-                test_path.append({"type": k, "name": steps[k]})
+                for k in steps:
+                    test_path.append({"type": k, "name": steps[k]})
 
-            yield CaseEvent.create(test_path, duration / 1000/1000 / 1000, status, None, "\n".join(stderr), None, None)
+                yield CaseEvent.create(test_path, duration / 1000/1000 / 1000, status, None, "\n".join(stderr), None, None)
 
 
 def _find_test_file_from_report_file(base_path: str, report: str) -> Optional[Path]:
