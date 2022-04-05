2ec1d0f57c83c81346af77fa177ebc71ee47a54a
shibuiwilliam <shibuiyusuke@gmail.com>
2022-04-05 12:39:55 +0900

    test pypi publish

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 
-def local_scheme(version) -> str:
+def local_scheme():
     """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
     to be able to upload to Test PyPI"""
-    return ""
+    def _local_scheme(version):
+        return ""
+    return {"local_scheme": _local_scheme}
 
 
 setup(
-    use_scm_version={"local_scheme": local_scheme},
+    use_scm_version=local_scheme
 )
