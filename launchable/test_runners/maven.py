161aab6f5657e463f6576e0282f07912b0d51c00
Ryosuke Yabuki <ryabuki@launchableinc.com>
2022-05-23 08:32:05 +0900

    add from-file option

--- launchable/test_runners/maven.py
+++ launchable/test_runners/maven.py
@@ -3,9 +3,10 @@ import click
 from . import launchable
 
 
-@click.argument('source_roots', required=True, nargs=-1)
+@click.option('--from-file', 'from_files', required=False, multiple=True, type=click.Path(exists=True))
+@click.argument('source_roots', required=False, nargs=-1)
 @launchable.subset
-def subset(client, source_roots):
+def subset(client, source_roots, from_files):
     def file2test(f: str):
         if f.endswith('.java') or f.endswith(".scala") or f.endswith(".kt"):
             f = f[:f.rindex('.')]   # remove extension
@@ -15,8 +16,17 @@ def subset(client, source_roots):
         else:
             return None
 
-    for root in source_roots:
-        client.scan(root, '**/*', file2test)
+    if len(from_files) > 0:
+        for file in from_files:
+            with open(file, 'r') as f:
+                lines = f.readlines()
+                for l in lines:
+                    client.test_paths.append(
+                        [{"type": "class", "name": l.strip()}])
+    else:
+
+        for root in source_roots:
+            client.scan(root, '**/*', file2test)
 
     client.run()
 
