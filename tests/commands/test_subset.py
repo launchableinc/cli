3f860009a2da35fb3d8fc1adf2ea5259202e4d74
Ryosuke Yabuki <ryabuki@launchableinc.com>
2022-07-20 15:41:40 +0900

    add test for --get-tests-from-previous-full-runs

--- tests/commands/test_subset.py
+++ tests/commands/test_subset.py
@@ -10,7 +10,6 @@ class SubsetTest(CliTestCase):
     @responses.activate
     @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
     def test_subset_with_observation_mode(self):
-
         pipe = "test_1.py\ntest_2.py\ntest_3.py\ntest_4.py"
         mock_json_response = {
             "testPaths": [
@@ -51,8 +50,8 @@ class SubsetTest(CliTestCase):
         observation_mode_rest = tempfile.NamedTemporaryFile(delete=False)
         result = self.cli("subset", "--target", "30%", "--session",
                           self.session, "--rest", observation_mode_rest.name, "--observation", "file", input=pipe, mix_stderr=False)
-
         self.assertEqual(result.exit_code, 0)
+
         self.assertEqual(
             result.stdout, "test_1.py\ntest_2.py\ntest_3.py\ntest_4.py\n")
         self.assertEqual(
@@ -62,6 +61,40 @@ class SubsetTest(CliTestCase):
 
     @responses.activate
     @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_subset_with_get_tests_from_previous_full_runs(self):
+        responses.replace(responses.POST, "{}/intake/organizations/{}/workspaces/{}/subset".format(get_base_url(), self.organization, self.workspace),
+                          json={
+            "testPaths": [
+                [{"type": "file", "name": "test_aaa.py"}],
+                [{"type": "file", "name": "test_bbb.py"}],
+                [{"type": "file", "name": "test_ccc.py"}],
+            ],
+            "rest": [
+                [{"type": "file", "name": "test_eee.py"}],
+                [{"type": "file", "name": "test_fff.py"}],
+                [{"type": "file", "name": "test_ggg.py"}],
+            ],
+            "subsettingId": 123,
+            "summary": {
+                "subset": {"duration": 10, "candidates": 3, "rate": 50},
+                "rest": {"duration": 10, "candidates": 3, "rate": 50}
+            },
+        }, status=200)
+
+        rest = tempfile.NamedTemporaryFile(delete=False)
+        result = self.cli("subset", "--target", "30%", "--session",
+                          self.session, "--rest", rest.name, "--get-tests-from-previous-full-runs",  "file", mix_stderr=False)
+
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(
+            result.stdout, "test_aaa.py\ntest_bbb.py\ntest_ccc.py\n")
+        self.assertEqual(
+            rest.read().decode(), "test_eee.py\ntest_fff.py\ntest_ggg.py")
+        rest.close()
+        os.unlink(rest.name)
+
+    @ responses.activate
+    @ mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
     def test_subset_with_output_exclusion_rules(self):
         pipe = "test_aaa.py\ntest_111.py\ntest_bbb.py\ntest_222.py\ntest_ccc.py\ntest_333.py\n"
         responses.replace(responses.POST, "{}/intake/organizations/{}/workspaces/{}/subset".format(get_base_url(), self.organization, self.workspace),
