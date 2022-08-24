122e6653af2cdcc936e939642b3cb5e58faf117c
Masaya Suzuki <draftcode@gmail.com>
2022-08-23 21:12:22 -0700

    Handle go-junit-report v2 changes
    This absorbs the difference between go-junit-report v1 and v2. Making
    the data backward compatible with v1.

--- launchable/test_runners/go_test.py
+++ launchable/test_runners/go_test.py
@@ -1,6 +1,11 @@
 from . import launchable
+import glob
+import os
 import re
+import click
+from junitparser import TestCase, TestSuite  # type: ignore
 from ..utils.logger import Logger
+from ..testpath import TestPath
 
 
 @launchable.subset
@@ -32,6 +37,37 @@ def subset(client):
     client.run()
 
 
+@click.argument('source_roots', required=True, nargs=-1)
+@launchable.record.tests
+def record_tests(client, source_roots):
+    for root in source_roots:
+        match = False
+        for t in glob.iglob(root, recursive=True):
+            match = True
+            if os.path.isdir(t):
+                client.scan(t, "*.xml")
+            else:
+                client.report(t)
+
+        if not match:
+            click.echo("No matches found: {}".format(root), err=True)
+            return
+
+    default_path_builder = client.path_builder
+
+    def path_builder(case: TestCase, suite: TestSuite, report_file: str) -> TestPath:
+        tp = default_path_builder(case, suite, report_file)
+        for tpc in tp:
+            if 'type' in tpc and 'name' in tpc and tpc['type'] == 'class':
+                # go-junit-report v2 reports full package name. go-junit-report
+                # v1 reports only the last component of the package name. In
+                # order to make this backward compatible, we align this to v1.
+                tpc['name'] = tpc['name'].split('/')[-1]
+        return tp
+
+    client.path_builder = path_builder
+    client.run()
+
+
 split_subset = launchable.CommonSplitSubsetImpls(
     __name__, formatter=lambda x: "^{}$".format(x[1]['name']), seperator='|').split_subset()
-record_tests = launchable.CommonRecordTestImpls(__name__).report_files()
