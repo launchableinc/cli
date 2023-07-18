cc08a722991b68f8cce8040a37a5ef0a7d7432c4
Konboi <ryosuke.yabuki@gmail.com>
2023-07-18 14:31:27 +0900

    Fix separator and add test

--- tests/test_runners/test_dotnet.py
+++ tests/test_runners/test_dotnet.py
@@ -94,6 +94,84 @@ class DotnetTest(CliTestCase):
         output = "FullyQualifiedName!=rocket_car_dotnet.ExampleTest.TestAdd&FullyQualifiedName!=rocket_car_dotnet.ExampleTest.TestDiv\n"  # noqa: E501
         self.assertEqual(result.output, output)
 
+    @responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_subset_with_bare_option(self):
+        mock_response = {
+            "testPaths": [
+                [
+                    {"type": "Assembly", "name": "rocket-car-dotnet.dll"},
+                    {"type": "TestSuite", "name": "rocket_car_dotnet"},
+                    {"type": "TestSuite", "name": "ExampleTest"},
+                    {"type": "TestCase", "name": "TestSub"},
+                ],
+                [
+                    {"type": "Assembly", "name": "rocket-car-dotnet.dll"},
+                    {"type": "TestSuite", "name": "rocket_car_dotnet"},
+                    {"type": "TestSuite", "name": "ExampleTest"},
+                    {"type": "TestCase", "name": "TestMul"},
+                ],
+            ],
+            "rest": [
+                [
+                    {"type": "Assembly", "name": "rocket-car-dotnet.dll"},
+                    {"type": "TestSuite", "name": "rocket_car_dotnet"},
+                    {"type": "TestSuite", "name": "ExampleTest"},
+                    {"type": "TestCase", "name": "TestAdd"},
+                ],
+                [
+                    {"type": "Assembly", "name": "rocket-car-dotnet.dll"},
+                    {"type": "TestSuite", "name": "rocket_car_dotnet"},
+                    {"type": "TestSuite", "name": "ExampleTest"},
+                    {"type": "TestCase", "name": "TestDiv"},
+                ],
+            ],
+            "testRunner": "dotnet",
+            "subsettingId": 123,
+            "summary": {
+                "subset": {"duration": 25, "candidates": 1, "rate": 25},
+                "rest": {"duration": 78, "candidates": 3, "rate": 75}
+            },
+            "isObservation": False,
+        }
+
+        responses.replace(responses.POST, "{}/intake/organizations/{}/workspaces/{}/subset".format(
+            get_base_url(),
+            self.organization,
+            self.workspace),
+            json=mock_response,
+            status=200)
+
+        result = self.cli(
+            'subset',
+            '--target',
+            '25%',
+            '--session',
+            self.session,
+            '--get-tests-from-previous-sessions',
+            'dotnet',
+            '--bare',
+            mix_stderr=False)
+        self.assertEqual(result.exit_code, 0)
+
+        output = "rocket_car_dotnet.ExampleTest.TestSub\nrocket_car_dotnet.ExampleTest.TestMul\n"
+        self.assertEqual(result.output, output)
+
+        result = self.cli(
+            'subset',
+            '--target',
+            '25%',
+            '--session',
+            self.session,
+            '--get-tests-from-previous-sessions',
+            '--output-exclusion-rules',
+            'dotnet',
+            '--bare',
+            mix_stderr=False)
+        self.assertEqual(result.exit_code, 0)
+        output = "rocket_car_dotnet.ExampleTest.TestAdd\nrocket_car_dotnet.ExampleTest.TestDiv\n"
+        self.assertEqual(result.output, output)
+
     @ignore_warnings
     @responses.activate
     @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
