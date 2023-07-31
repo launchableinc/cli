64f49ff9b7f806a213c58515ee1c372f71ced6a0
Konboi <ryosuke.yabuki@gmail.com>
2023-07-31 13:43:26 +0900

    rm 3.5 just in case

--- tests/utils/test_key_value_type.py
+++ tests/utils/test_key_value_type.py
@@ -6,8 +6,8 @@ from launchable.utils.key_value_type import normalize_key_value_types
 class NormalizeKeyValueTest(TestCase):
     def test_normalize_key_value_types(self):
         # <click8.0
-        result = normalize_key_value_types((("os", "ubuntu"), ("python", "3.5")))
-        self.assertEqual(result, [("os", "ubuntu"), ("python", "3.5")])
+        result = normalize_key_value_types((("os", "ubuntu"), ("python", "3.6")))
+        self.assertEqual(result, [("os", "ubuntu"), ("python", "3.6")])
 
         # >=click8.0
         result = normalize_key_value_types(("('os', 'ubuntu')", "('python', '3.8')"))
