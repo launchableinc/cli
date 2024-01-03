906fb99419bc02f510666c8232b6d8f33bcafb14
Kohsuke Kawaguchi <kk@kohsuke.org>
2024-01-03 09:18:52 -0800

    Fixed a broken link

--- launchable/commands/helper.py
+++ launchable/commands/helper.py
@@ -44,7 +44,8 @@ def require_build() -> str:
                 "No saved build name found.\n"
                 "To fix this, run `launchable record build`.\n"
                 "If you already ran this command on a different machine, use the --session option. "
-                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts",
+                "See https://www.launchableinc.com/docs/sending-data-to-launchable/using-the-launchable-cli/"
+                "recording-test-results-with-the-launchable-cli/managing-complex-test-session-layouts/",
                 fg="yellow"))
     return b
 
@@ -106,7 +107,8 @@ def find_or_create_session(
                 "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
                 "Make sure to run `launchable record build --name {}` before you run this command.\n"
                 "If you already recorded this build on a different machine, use the --session option instead of --build. "
-                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
+                "See https://www.launchableinc.com/docs/sending-data-to-launchable/using-the-launchable-cli/"
+                "recording-test-results-with-the-launchable-cli/managing-complex-test-session-layouts/".format(
                     build_name, saved_build_name, build_name), fg="yellow", ))
 
     session_id = read_session(saved_build_name)
