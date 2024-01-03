8c69c54f7f7cccfda2c87ae6e901efe2e9c09541
Kohsuke Kawaguchi <kk@kohsuke.org>
2024-01-03 09:16:09 -0800

    Eliminating what appears to be a merge error or copy/paste error
    Introduced by 545f5a9c78e4cb24c6588cde392a25fa068b044c

--- launchable/commands/helper.py
+++ launchable/commands/helper.py
@@ -114,32 +114,18 @@ def find_or_create_session(
         _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
         return session_id
 
-    if build_name and saved_build_name != build_name:
-        raise click.UsageError(
-            click.style(
-                "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
-                "Make sure to run `launchable record build --name {}` before you run this command.\n"
-                "If you already recorded this build on a different machine, use the --session option instead of --build. "
-                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
-                    build_name, saved_build_name, build_name), fg="yellow", ))
-
-    session_id = read_session(saved_build_name)
-    if session_id:
-        _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
-        return session_id
-    else:
-        context.invoke(
-            session_command,
-            build_name=saved_build_name,
-            save_session_file=True,
-            print_session=False,
-            flavor=flavor,
-            is_observation=is_observation,
-            links=links,
-            is_no_build=is_no_build,
-            lineage=lineage,
-        )
-        return read_session(saved_build_name)
+    context.invoke(
+        session_command,
+        build_name=saved_build_name,
+        save_session_file=True,
+        print_session=False,
+        flavor=flavor,
+        is_observation=is_observation,
+        links=links,
+        is_no_build=is_no_build,
+        lineage=lineage,
+    )
+    return read_session(saved_build_name)
 
 
 def _check_observation_mode_status(session: str, is_observation: bool, tracking_client: TrackingClient):

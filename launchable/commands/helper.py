launchableinc/cli

commit 545f5a9c78e4cb24c6588cde392a25fa068b044c

[ENG-474] Added the attachment command
This new command attaches a log file to a test session

launchable/commands/helper.py
@@ -9,6 +9,46 @@ from ..utils.launchable_client import LaunchableClient
 from ..utils.session import read_build, read_session
 
 
+def require_session(
+        session: Optional[str],
+) -> Optional[str]:
+    """Ascertain the contextual test session to operate a CLI command for. If one doesn't exit, fail.
+
+    1. If the user explicitly provides the session id via the `--session` option
+    2. If the user gives no options, the current session ID is read from the session file tied to $PWD.
+       See https://github.com/launchableinc/cli/pull/342
+    """
+    if session:
+        return session
+
+    session = read_session(require_build())
+    if session:
+        return session
+
+    raise click.UsageError(
+        click.style(
+            "No saved test session found.\n"
+            "If you already created a test session on a different machine, use the --session option. "
+            "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts",
+            fg="yellow"))
+
+
+def require_build() -> str:
+    """
+    Like read_build() but fail if a build doesn't exist
+    """
+    b = read_build()
+    if not b:
+        raise click.UsageError(
+            click.style(
+                "No saved build name found.\n"
+                "To fix this, run `launchable record build`.\n"
+                "If you already ran this command on a different machine, use the --session option. "
+                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts",
+                fg="yellow"))
+    return b
+
+
 def find_or_create_session(
     context: click.core.Context,
     session: Optional[str],
@@ -58,43 +98,48 @@ def find_or_create_session(
         saved_build_name = read_build()
         return read_session(str(saved_build_name))
 
-    saved_build_name = read_build()
-    if not saved_build_name:
+    saved_build_name = require_build()
+
+    if build_name and saved_build_name != build_name:
         raise click.UsageError(
             click.style(
-                "No saved build name found.\n"
-                "To fix this, run `launchable record build`.\n"
-                "If you already ran this command on a different machine, use the --session option. "
-                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts",
-                fg="yellow"))
+                "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
+                "Make sure to run `launchable record build --name {}` before you run this command.\n"
+                "If you already recorded this build on a different machine, use the --session option instead of --build. "
+                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
+                    build_name, saved_build_name, build_name), fg="yellow", ))
 
+    session_id = read_session(saved_build_name)
+    if session_id:
+        _check_observation_mode_status(session_id, is_observation)
+        return session_id
+
+    if build_name and saved_build_name != build_name:
+        raise click.UsageError(
+            click.style(
+                "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
+                "Make sure to run `launchable record build --name {}` before you run this command.\n"
+                "If you already recorded this build on a different machine, use the --session option instead of --build. "
+                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
+                    build_name, saved_build_name, build_name), fg="yellow", ))
+
+    session_id = read_session(saved_build_name)
+    if session_id:
+        _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
+        return session_id
     else:
-        if build_name and saved_build_name != build_name:
-            raise click.UsageError(
-                click.style(
-                    "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
-                    "Make sure to run `launchable record build --name {}` before you run this command.\n"
-                    "If you already recorded this build on a different machine, use the --session option instead of --build. "
-                    "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
-                        build_name, saved_build_name, build_name), fg="yellow", ))
-
-        session_id = read_session(saved_build_name)
-        if session_id:
-            _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
-            return session_id
-        else:
-            context.invoke(
-                session_command,
-                build_name=saved_build_name,
-                save_session_file=True,
-                print_session=False,
-                flavor=flavor,
-                is_observation=is_observation,
-                links=links,
-                is_no_build=is_no_build,
-                lineage=lineage,
-            )
-            return read_session(saved_build_name)
+        context.invoke(
+            session_command,
+            build_name=saved_build_name,
+            save_session_file=True,
+            print_session=False,
+            flavor=flavor,
+            is_observation=is_observation,
+            links=links,
+            is_no_build=is_no_build,
+            lineage=lineage,
+        )
+        return read_session(saved_build_name)
 
 
 def _check_observation_mode_status(session: str, is_observation: bool, tracking_client: TrackingClient):

launchable/commands/record/__init__.py
@@ -2,6 +2,7 @@ import click
 
 from launchable.utils.click import GroupWithAlias
 
+from .attachment import attachment
 from .build import build
 from .commit import commit
 from .session import session
@@ -19,3 +20,4 @@ record.add_command(tests)
 # for backward compatibility
 record.add_alias('test', tests)  # type: ignore
 record.add_command(session)
+record.add_command(attachment)

launchable/commands/record/attachment.py
@@ -1,0 +1,38 @@
+import os
+from typing import Optional
+
+import click
+
+from ...utils.env_keys import REPORT_ERROR_KEY
+from ...utils.http_client import LaunchableClient
+from ..helper import require_session
+
+
+@click.command()
+@click.option(
+    '--session',
+    'session',
+    help='Test session ID',
+    type=str,
+)
+@click.argument('attachments', nargs=-1)  # type=click.Path(exists=True)
+def attachment(
+        attachments,
+        session: Optional[str] = None
+):
+    try:
+        session = require_session(session)
+
+        client = LaunchableClient()
+        for a in attachments:
+            click.echo("Sending {}".format(a))
+            with open(a, mode='rb') as f:
+                res = client.request(
+                    "post", "{}/attachment".format(session), compress=True, payload=f,
+                    additional_headers={"Content-Disposition": "attachment;filename=\"{}\"".format(a)})
+                res.raise_for_status()
+    except Exception as e:
+        if os.getenv(REPORT_ERROR_KEY):
+            raise e
+        else:
+            click.echo(e)

launchable/utils/http_client.py
@@ -2,7 +2,7 @@ import gzip
 import json
 import os
 import platform
-from typing import Dict, Optional, Tuple
+from typing import IO, BinaryIO, Dict, Optional, Tuple, Union
 
 from requests import Session
 from requests.adapters import HTTPAdapter
@@ -12,6 +12,7 @@ from launchable.version import __version__
 
 from .authentication import authentication_headers
 from .env_keys import BASE_URL_KEY
+from .gzipgen import compress as gzipgen_compress
 from .logger import AUDIT_LOG_FORMAT, Logger
 
 DEFAULT_BASE_URL = "https://api.mercury.launchableinc.com"
@@ -61,14 +62,17 @@ class _HttpClient:
         self,
         method: str,
         path: str,
-        payload: Optional[Dict] = None,
+        payload: Union[Dict, BinaryIO, type(None)] = None,
         params: Optional[Dict] = None,
         timeout: Tuple[int, int] = (5, 60),
         compress: bool = False,
+        additional_headers: Dict = None,
     ):
         url = _join_paths(self.base_url, path)
 
         headers = self._headers(compress)
+        if additional_headers:
+            headers = {**headers, **additional_headers}
 
         Logger().audit(AUDIT_LOG_FORMAT.format("(DRY RUN) " if self.dry_run else "", method, url, headers, payload))
 
@@ -81,6 +85,8 @@ class _HttpClient:
 
         data = _build_data(payload, compress)
 
+        # the 'data' argument accepts generator. whenever we can potentially send a large amount of data,
+        # we want to use generator to stream data
         response = self.session.request(method, url, headers=headers, timeout=timeout, data=data, params=params)
         Logger().debug(
             "received response status:{} message:{} headers:{}".format(response.status_code, response.reason,
@@ -107,15 +113,33 @@ class _HttpClient:
         return {**h, **authentication_headers()}
 
 
-def _build_data(payload, compress):
+def _file_to_generator(f: IO, chunk_size=4096):
+    """
+    Returns a generator that reads from a given file-like object
+    """
+    while True:
+        data = f.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
+def _build_data(payload: Union[BinaryIO, Dict, type(None)], compress: bool):
     if payload is None:
         return None
-
-    encoded = json.dumps(payload).encode()
-    if compress:
-        return gzip.compress(encoded)
-
-    return encoded
+    if isinstance(payload, dict):
+        encoded = json.dumps(payload).encode()
+        if compress:
+            return gzip.compress(encoded)
+        else:
+            return encoded
+    else:
+        # payload is BinaryIO
+        if compress:
+            # this produces a generator
+            return gzipgen_compress(_file_to_generator(payload))
+        else:
+            return payload
 
 
 def _join_paths(*components):

commit 8c69c54f7f7cccfda2c87ae6e901efe2e9c09541

Eliminating what appears to be a merge error or copy/paste error
Introduced by 545f5a9c78e4cb24c6588cde392a25fa068b044c

launchable/commands/helper.py
@@ -114,32 +114,18 @@ def find_or_create_session(
         _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
         return session_id
 
-    if build_name and saved_build_name != build_name:
-        raise click.UsageError(
-            click.style(
-                "The build name you provided ({}) is different from the last build name recorded on this machine ({}).\n"
-                "Make sure to run `launchable record build --name {}` before you run this command.\n"
-                "If you already recorded this build on a different machine, use the --session option instead of --build. "
-                "See https://docs.launchableinc.com/sending-data-to-launchable/managing-complex-test-session-layouts".format(
-                    build_name, saved_build_name, build_name), fg="yellow", ))
-
-    session_id = read_session(saved_build_name)
-    if session_id:
-        _check_observation_mode_status(session_id, is_observation, tracking_client=tracking_client)
-        return session_id
-    else:
-        context.invoke(
-            session_command,
-            build_name=saved_build_name,
-            save_session_file=True,
-            print_session=False,
-            flavor=flavor,
-            is_observation=is_observation,
-            links=links,
-            is_no_build=is_no_build,
-            lineage=lineage,
-        )
-        return read_session(saved_build_name)
+    context.invoke(
+        session_command,
+        build_name=saved_build_name,
+        save_session_file=True,
+        print_session=False,
+        flavor=flavor,
+        is_observation=is_observation,
+        links=links,
+        is_no_build=is_no_build,
+        lineage=lineage,
+    )
+    return read_session(saved_build_name)
 
 
 def _check_observation_mode_status(session: str, is_observation: bool, tracking_client: TrackingClient):