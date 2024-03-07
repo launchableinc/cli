3c90721523fa462772dfbb579c27722c80ad7dea
Konboi <ryosuke.yabuki@gmail.com>
2024-03-07 09:55:44 +0900

    1. Update to print summary 2. Introduce TestResult and TestResults class for easy to handle them 3. Introduce Abstract class to support JSON view

--- launchable/commands/inspect/tests.py
+++ launchable/commands/inspect/tests.py
@@ -1,12 +1,97 @@
 import os
 import sys
+from abc import ABCMeta, abstractmethod
 from http import HTTPStatus
+from typing import List
 
 import click
 from tabulate import tabulate
+from typing_extensions import Self
 
 from ...utils.env_keys import REPORT_ERROR_KEY
 from ...utils.launchable_client import LaunchableClient
+
+
+class TestResult(object):
+    def __init__(self, result: dict):
+        self._status = result.get("status", "")
+        self._duration_sec = result.get("duration", 0.0)
+        self._created_at = result.get("createdAt", None)
+        self._test_path = "#".join([path["type"] + "=" + path["name"]
+                                   for path in result["testPath"] if path.keys() >= {"type", "name"}])
+
+
+class TestResults(object):
+    def __init__(self, results: List[TestResult] = []):
+        self._results = results
+
+    def add(self, result: TestResult):
+        self._results.append(result)
+
+    def list(self) -> List[TestResult]:
+        return self._results
+
+    def total_duration_sec(self) -> float:
+        return sum([result._duration_sec for result in self._results])
+
+    def total_duration_min(self) -> float:
+        return (sum([result._duration_sec for result in self._results]) / 60)
+
+    def total_count(self) -> int:
+        return len(self._results)
+
+    def filter_by_status(self, status: str) -> Self:
+        return TestResults([result for result in self._results if result._status == status])
+
+
+class AbstractTestResultDisplay(metaclass=ABCMeta):
+    def __init__(self, results: TestResults):
+        self._results = results
+
+    @abstractmethod
+    def display(self):
+        raise NotImplementedError("display method is not implemented")
+
+
+class JSONTestResultDisplay(AbstractTestResultDisplay):
+    def __init__(self, results: TestResults):
+        super().__init__(results)
+
+    def display(self):
+        raise NotImplementedError("TODO")
+
+
+class StdOutTestResultDisplay(AbstractTestResultDisplay):
+    def __init__(self, results: TestResults):
+        super().__init__(results)
+
+    def display(self):
+        header = ["Test Path",
+                  "Duration (sec)", "Status", "Uploaded At"]
+        rows = []
+        for result in self._results.list():
+            rows.append(
+                [
+                    result._test_path,
+                    result._duration_sec,
+                    result._status,
+                    result._created_at,
+                ]
+            )
+        click.echo(tabulate(rows, header, tablefmt="github", floatfmt=".2f"))
+
+        summary_header = ["Summary", "Report Count", "Total Duration (min)"]
+        summary_rows = [
+            ["Total", self._results.total_count(),
+             self._results.total_duration_min()],
+            ["Success", self._results.filter_by_status("SUCCESS").total_count(),
+             self._results.filter_by_status("SUCCESS").total_duration_min()],
+            ["Failure", self._results.filter_by_status("FAILURE").total_count(),
+             self._results.filter_by_status("FAILURE").total_duration_min()],
+            ["Skip", self._results.filter_by_status("SKIPPED").total_count(),
+             self._results.filter_by_status("SKIPPED").total_duration_min()],]
+
+        click.echo(tabulate(summary_rows, summary_header, tablefmt="grid", floatfmt=["", ".0f", ".2f"]))
 
 
 @click.command()
@@ -43,20 +128,10 @@ def tests(context: click.core.Context, test_session_id: int):
 
         return
 
-    header = ["Test Path",
-              "Duration (sec)", "Status", "Uploaded At"]
-
-    rows = []
+    test_results = TestResults()
     for result in results:
         if result.keys() >= {"testPath"}:
-            rows.append(
-                [
-                    "#".join([path["type"] + "=" + path["name"]
-                             for path in result["testPath"] if path.keys() >= {"type", "name"}]),
-                    result.get("duration", 0.0),
-                    result.get("status", ""),
-                    result.get("createdAt", None),
-                ]
-            )
+            test_results.add(TestResult(result))
 
-    click.echo(tabulate(rows, header, tablefmt="github", floatfmt=".2f"))
+    stdout_display = StdOutTestResultDisplay(test_results)
+    stdout_display.display()
