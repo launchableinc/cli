324d1ac06b21506e89fd3ebe5dd42c0611ce6781
Ryosuke Yabuki <ryabuki@cloudbees.com>
2024-11-15 14:53:47 +0900

    implement flutter plugin

--- launchable/test_runners/flutter.py
+++ launchable/test_runners/flutter.py
@@ -1,12 +1,12 @@
 import json
+import pathlib
 from typing import Dict, Generator, List, Optional
 import click
-
+from pathlib import Path
 
 from launchable.commands.record.case_event import CaseEvent
 from launchable.testpath import FilePathNormalizer
 from . import launchable
-
 
 
 FLUTTER_FILE_EXT = "_test.dart"
@@ -17,13 +17,13 @@ FLUTTER_TEST_RESULT_FAILURE = "error"
 
 class TestCase:
     def __init__(self, id: int, name: str, is_skipped: bool = False):
-        self._id = id
-        self._name = name
-        self._is_skipped = is_skipped
-        self._status = None
-        self._stdout = ""
-        self._stderr = ""
-        self._duration_sec = 0
+        self._id: int = id
+        self._name: str = name
+        self._is_skipped: bool = is_skipped
+        self._status: str = ""
+        self._stdout: str = ""
+        self._stderr: str = ""
+        self._duration_sec: float = 0
 
     @property
     def id(self):
@@ -34,7 +34,7 @@ class TestCase:
         return self._name
 
     @property
-    def status(self) -> CaseEvent:
+    def status(self) -> int:  # status code see: case_event.py
         if self._is_skipped:
             return CaseEvent.TEST_SKIPPED
         elif self._status == FLUTTER_TEST_RESULT_SUCCESS:
@@ -79,47 +79,39 @@ class TestSuite:
         self._id = id
         self._platform = platform
         self._path = path
-        self._test_cases: List[TestCase] = []
+        self._test_cases: Dict[int, TestCase] = {}
 
     def _get_test_case(self, id: int) -> Optional[TestCase]:
-        if id is None:
-            return
-
-        for c in self._test_cases:
-            if c.id == id:
-                return c
+        return self._test_cases.get(id)
 
 
 class ReportParser:
-    def __init__(self, client):
-        self.client = client
-        self.file_path_normalizer = FilePathNormalizer(base_path=client.base_path,
-                                                       no_base_path_inference=client.no_base_path_inference)
-        self._suites: List[TestSuite] = []
+    def __init__(self, file_path_normalizer: FilePathNormalizer):
+        self.file_path_normalizer = file_path_normalizer
+        self._suites: Dict[int, TestSuite] = {}
 
     def _get_suite(self, suite_id: int) -> Optional[TestSuite]:
-        if suite_id is None:
-            return
-
-        for s in self._suites:
-            if s._id == suite_id:
-                return s
+        return self._suites.get(suite_id)
 
     def _get_test(self, test_id: int) -> Optional[TestCase]:
         if test_id is None:
-            return
+            return None
 
-        for s in self._suites:
-            for c in s._test_cases:
-                if c.id == test_id:
-                    return c
+        for s in self._suites.values():
+            c = s._get_test_case(test_id)
+            if c is not None:
+                return c
 
-    def _events(self) -> List[CaseEvent]:
+        return None
+
+    def _events(self) -> List:
         events = []
-        for s in self._suites:
-            for c in s._test_cases:
+        for s in self._suites.values():
+            for c in s._test_cases.values():
                 events.append(CaseEvent.create(
-                    test_path=[{"type": "file", "name": s._path, }, {"type": "testcase", "name": c.name}],
+                    test_path=[
+                        {"type": "file", "name": pathlib.Path(self.file_path_normalizer.relativize(s._path)).as_posix()},
+                        {"type": "testcase", "name": c.name}],
                     duration_secs=c.duration,
                     status=c.status,
                     stdout=c.stdout,
@@ -136,9 +128,7 @@ class ReportParser:
                 data = json.loads(j)
                 self._parse_json(data)
 
-        print("come on")
         for event in self._events():
-            print("event~~~",event)
             yield event
 
     def _parse_json(self, data: Dict):
@@ -149,39 +139,48 @@ class ReportParser:
         data_type = data.get("type")
         if data_type is None:
             return
-
         elif data_type == "suite":
             suite_data = data.get("suite")
             if suite_data is None:
                 # it's might be invalid suite data
                 return
 
-            self._suites.append(
-                TestSuite(suite_data.get("id"), suite_data.get("platform"), suite_data.get("path"))
-            )
+            suite_id = suite_data.get("id")
+            if self._get_suite(suite_data.get("id")) is not None:
+                # already recorded
+                return
+
+            self._suites[suite_id] = TestSuite(suite_id, suite_data.get("platform"), suite_data.get("path"))
         elif data_type == "testStart":
             test_data = data.get("test")
+
             if test_data is None:
                 # it's might be invalid test data
                 return
-
             if test_data.get("line") is None:
-                # Still set up test case
+                # Still set up test case, should skip
                 return
 
             suite_id = test_data.get("suiteID")
             suite = self._get_suite(suite_id)
+
             if suite_id is None or suite is None:
                 click.echo(click.style(
                     "Warning: Cannot find a parent test suite (id: {}). So won't send test result of {}".format(
                         suite_id, test_data.get("name")), fg='yellow'), err=True)
                 return
 
-            id = test_data.get("id")
+            test_id = test_data.get("id")
+            test = self._get_test(test_id)
+            if test is not None:
+                # already recorded
+                return
+
             name = test_data.get("name")
             metadata = test_data.get("metadata", {})
             is_skipped = metadata.get("skip", False)
-            suite._test_cases.append(TestCase(id, name, is_skipped))
+            suite._test_cases[test_id] = TestCase(test_id, name, is_skipped)
+            return
         elif data_type == "testDone":
             test_id = data.get("testID")
             test = self._get_test(test_id)
@@ -190,9 +189,9 @@ class ReportParser:
                 return
 
             test.status = data.get("result", "success")  # safe fallback
-            duration_msec = data.get("duration", 0)
+            duration_msec = data.get("time", 0)
             test.duration = duration_msec / 1000  # to sec
-
+            return
         elif data_type == "error":
             test_id = data.get("testID")
             test = self._get_test(test_id)
@@ -202,7 +201,7 @@ class ReportParser:
                     err=True)
                 return
             test.stderr += ("\n" if test.stderr else "") + data.get("error", "")
-
+            return
         elif data_type == "print":
             test_id = data.get("testID")
             test = self._get_test(test_id)
@@ -213,6 +212,7 @@ class ReportParser:
                 return
 
             test.stdout += ("\n" if test.stdout else "") + data.get("message", "")
+            return
         else:
             return
 
@@ -220,10 +220,11 @@ class ReportParser:
 @click.argument('reports', required=True, nargs=-1)
 @launchable.record.tests
 def record_tests(client, reports):
-    for r in reports:
-        client.report(r)
-    client.parse_func = ReportParser(client).parse_func
-    client.run()
+    file_path_normalizer = FilePathNormalizer(base_path=client.base_path, no_base_path_inference=client.no_base_path_inference)
+    client.parse_func = ReportParser(file_path_normalizer).parse_func
+
+    launchable.CommonRecordTestImpls.load_report_files(client=client, source_roots=reports)
+
 
 
 subset = launchable.CommonSubsetImpls(__name__).scan_files('*.dart')
