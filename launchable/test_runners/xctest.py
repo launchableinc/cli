6f9ebcb50b2e2b5ec78c9bb54760edcf5aa9ab3b
Konboi <ryosuke.yabuki@gmail.com>
2025-06-06 13:44:36 +0900

    support subset

--- launchable/test_runners/xctest.py
+++ launchable/test_runners/xctest.py
@@ -24,3 +24,29 @@ def record_tests(client, reports):
     for r in reports:
         client.report(r)
         client.run()
+
+
+@launchable.subset
+def subset(client):
+    if not client.is_get_tests_from_previous_sessions or not client.is_output_exclusion_rules:
+        click.echo(
+            click.style(
+                "XCTest profile only supports the subset with `--get-tests-from-previous-sessions` and `--output-exclusion-rules` options",  # noqa: E501
+                fg="red"),
+            err=True,
+        )
+
+    def formatter(test_path: TestPath) -> str:
+        if len(test_path) == 0:
+            return ""
+
+        # only target case
+        if len(test_path) == 1:
+            return "-skip-testing:{}".format(test_path[0]['name'])
+
+        # default target/class format
+        return "-skip-testing:{}/{}".format(test_path[0]['name'], test_path[1]['name'])
+
+    client.formatter = formatter
+    client.separator = "\n"
+    client.run()
