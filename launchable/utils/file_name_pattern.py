d789de9f5dd89fbdaeaaf9c83aed3a752e1bb916
Konboi <ryosuke.yabuki@gmail.com>
2025-06-19 09:58:40 +0900

    Groovy lang works on JVM but we haven't supported it as jvm_test_pattern
    so, fixed the regexp to support groovy

--- launchable/utils/file_name_pattern.py
+++ launchable/utils/file_name_pattern.py
@@ -1,4 +1,4 @@
 import re
 
-# find *Test, *Tests, *TestCase + .java, .scala, .kt
-jvm_test_pattern = re.compile(r'^.*Test(?:Case|s)?\.(?:java|scala|kt)$')
+# find *Test, *Tests, *TestCase, *Spec + .java, .scala, .kt, .groovy
+jvm_test_pattern = re.compile(r'^.*(?:Test(?:Case|s)?|Spec)\.(?:java|scala|kt|groovy)$')
