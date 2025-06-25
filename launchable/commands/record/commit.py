23f16f3a61b88095cc44e6ed50e1550944269cd0
Konboi <ryosuke.yabuki@gmail.com>
2025-06-25 14:54:17 +0900

    fix commit command

--- launchable/commands/record/commit.py
+++ launchable/commands/record/commit.py
@@ -13,6 +13,7 @@ from ...app import Application
 from ...utils.commands import Command
 from ...utils.commit_ingester import upload_commits
 from ...utils.env_keys import COMMIT_TIMEOUT, REPORT_ERROR_KEY
+from ...utils.fail_fast_mode import set_fail_fast_mode, warning_and_exit_if_fail_fast_mode
 from ...utils.git_log_parser import parse_git_log
 from ...utils.http_client import get_base_url
 from ...utils.java import cygpath, get_java_command
@@ -56,10 +57,10 @@ def commit(ctx, source: str, executable: bool, max_days: int, scrub_pii: bool, i
 
     tracking_client = TrackingClient(Command.COMMIT, app=ctx.obj)
     client = LaunchableClient(tracking_client=tracking_client, app=ctx.obj)
-    is_fail_fast_mode = client.is_fail_fast_mode()
+    set_fail_fast_mode(client.is_fail_fast_mode())
 
     if import_git_log_output:
-        _import_git_log(import_git_log_output, ctx.obj, is_fail_fast_mode)
+        _import_git_log(import_git_log_output, ctx.obj)
         return
 
     # Commit messages are not collected in the default.
@@ -83,15 +84,9 @@ def commit(ctx, source: str, executable: bool, max_days: int, scrub_pii: bool, i
         if os.getenv(REPORT_ERROR_KEY):
             raise e
         else:
-            click.echo(click.style(
+            warning_and_exit_if_fail_fast_mode(
                 "Couldn't get commit history from `{}`. Do you run command root of git-controlled directory? "
-                "If not, please set a directory use by --source option."
-                .format(cwd),
-                fg='yellow'),
-                err=True)
-            print(e)
-            if is_fail_fast_mode:
-                sys.exit(1)
+                "If not, please set a directory use by --source option.\nerror: {}".format(cwd, e))
 
 
 def exec_jar(source: str, max_days: int, app: Application, is_collect_message: bool):
@@ -136,7 +131,7 @@ def exec_jar(source: str, max_days: int, app: Application, is_collect_message: b
     )
 
 
-def _import_git_log(output_file: str, app: Application, is_fail_fast_mode: bool = False):
+def _import_git_log(output_file: str, app: Application):
     try:
         with click.open_file(output_file) as fp:
             commits = parse_git_log(fp)
@@ -145,12 +140,7 @@ def _import_git_log(output_file: str, app: Application, is_fail_fast_mode: bool 
         if os.getenv(REPORT_ERROR_KEY):
             raise e
         else:
-            click.echo(
-                click.style("Failed to import the git-log output", fg='yellow'),
-                err=True)
-            print(e)
-            if is_fail_fast_mode:
-                sys.exit(1)
+            warning_and_exit_if_fail_fast_mode("Failed to import the git-log output\n error: {}".format(e))
 
 
 def _build_proxy_option(https_proxy: Optional[str]) -> List[str]:
