82d494a2bcd5e9902f6b2eaf7e92165d2f6861c1
Naoto Ono <nono@cloudbees.com>
2025-06-26 16:21:49 +0900

    refactor fail-fast validation logic

--- launchable/utils/fail_fast_mode.py
+++ launchable/utils/fail_fast_mode.py
@@ -30,70 +30,40 @@ def warning_and_exit_if_fail_fast_mode(message: str):
         sys.exit(1)
 
 
-class FailFastModeValidator:
-    def __init__(
-        self,
-        command: Command,
-        build: Optional[str] = None,
-        is_no_build: bool = False,
-        test_suite: Optional[str] = None,
-        session: Optional[str] = None,
-        links: Sequence[Tuple[str, str]] = (),
-        is_observation: bool = False,
-        flavor: Sequence[Tuple[str, str]] = (),
-    ):
-        self.command = command
-        self.build = build
-        self.is_no_build = is_no_build
-        self.test_suite = test_suite
-        self.session = session
-        self.links = links
-        self.is_observation = is_observation
-        self.flavor = flavor
-        self.errors: List[str] = []
+class FailFastContext:
+    command: Command
+    build: Optional[str] = None
+    is_no_build: bool = False
+    test_suite: Optional[str] = None
+    session: Optional[str] = None
+    links: Sequence[Tuple[str, str]] = ()
+    is_observation: bool = False
+    flavor: Sequence[Tuple[str, str]] = ()
 
-    def validate(self):
-        if not is_fail_fast_mode():
-            return
 
-        if self.command == Command.RECORD_SESSION:
-            self._validate_record_session()
-        if self.command == Command.SUBSET:
-            self._validate_subset()
-        if self.command == Command.RECORD_TESTS:
-            self._validate_record_tests()
-
-    def _validate_record_session(self):
-        # Now, there isn't any validation for the `record session` command in fail-fast mode.
+def fail_fast_validate(ctx: FailFastContext, fail_fast: bool = False):
+    if not is_fail_fast_mode():
         return
 
-    def _validate_require_session_option(self, cmd_name: str):
-        if self.session:
-            if self.test_suite:
-                self.errors.append("`--test-suite` option was ignored in the {} command. Add `--test-suite` option to the `record session` command instead.".format(cmd_name))  # noqa: E501
+    errors = []
 
-            if self.is_observation:
-                self.errors.append(
-                    "`--observation` was ignored in the {} command. Add `--observation` option to the `record session` command instead.".format(cmd_name))  # noqa: E501
+    # Now, there isn't any validation for the `record session` command in fail-fast mode.
+    if ctx.command in {Command.RECORD_TESTS, Command.SUBSET} and ctx.session:
+        cmd_name = ctx.command.value.replace("_", " ")
+        if ctx.test_suite:
+            errors.append(f"`--test-suite` is ignored in {cmd_name}. Add it to `record session` instead.")
+        if ctx.is_observation:
+            errors.append(f"`--observation` is ignored in {cmd_name}. Add it to `record session` instead.")
+        if ctx.flavor:
+            errors.append(f"`--flavor` is ignored in {cmd_name}. Add it to `record session` instead.")
+        if ctx.links:
+            errors.append(f"`--link` is ignored in {cmd_name}. Add it to `record session` instead.")
 
-            if len(self.flavor) > 0:
-                self.errors.append(
-                    "`--flavor` option was ignored in the {} command. Add `--flavor` option to the `record session` command instead.".format(cmd_name))  # noqa: E501
+    _exit_if_errors(errors)
 
-            if len(self.links) > 0:
-                self.errors.append(
-                    "`--link` option was ignored in the {} command. Add `link` option to the `record session` command instead.".format(cmd_name))  # noqa: E501
 
-    def _validate_subset(self):
-        self._validate_require_session_option("subset")
-        self._print_errors()
-
-    def _validate_record_tests(self):
-        self._validate_require_session_option("record tests")
-        self._print_errors()
-
-    def _print_errors(self):
-        if len(self.errors) > 0:
-            msg = "\n".join(map(lambda x: click.style(x, fg='red'), self.errors))
-            click.echo(msg, err=True)
-            sys.exit(1)
+def _exit_if_errors(errors: List[str]):
+    if len(errors) > 0:
+        msg = "\n".join(map(lambda x: click.style(x, fg='red'), errors))
+        click.echo(msg, err=True)
+        sys.exit(1)
