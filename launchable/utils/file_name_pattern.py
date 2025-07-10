a1fd28425c4dcffdd990719680dcb3f2adb99206
Konboi <ryosuke.yabuki@gmail.com>
2025-07-10 17:03:00 +0900

    make the matching rule more flexible

--- launchable/utils/file_name_pattern.py
+++ launchable/utils/file_name_pattern.py
@@ -1,4 +1,4 @@
 import re
 
-# find *Test, *Tests, *TestCase, *Spec + .java, .scala, .kt, .groovy
-jvm_test_pattern = re.compile(r'^.*(?:Test(?:Case|s)?|Spec)\.(?:java|scala|kt|groovy)$')
+# find *Test, *Tests, *TestCase, *Spec, *Suite + .java, .scala, .kt, .groovy
+jvm_test_pattern = re.compile(r'^.*(?:Test(?:Case|s)?|Spec|Suite)\.(?:java|scala|kt|groovy)$')
