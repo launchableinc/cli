73d5c174d208f12141b3c260298fc5d7ed44c286
ninjinkun <ninjin@mac.com>
2025-08-20 15:12:36 +0900

    fix: import paths

--- tests/utils/test_fail_fast_mode.py
+++ tests/utils/test_fail_fast_mode.py
@@ -1,8 +1,8 @@
 import io
 from contextlib import contextmanager, redirect_stderr
 
-from launchable.utils.commands import Command
-from launchable.utils.fail_fast_mode import FailFastModeValidateParams, fail_fast_mode_validate
+from smart_tests.utils.commands import Command
+from smart_tests.utils.fail_fast_mode import FailFastModeValidateParams, fail_fast_mode_validate
 from tests.cli_test_case import CliTestCase
 
 
@@ -30,7 +30,7 @@ class FailFastModeTest(CliTestCase):
 
 @contextmanager
 def tmp_set_fail_fast_mode(enabled: bool):
-    from launchable.utils.fail_fast_mode import _fail_fast_mode_cache, set_fail_fast_mode
+    from smart_tests.utils.fail_fast_mode import _fail_fast_mode_cache, set_fail_fast_mode
     original = _fail_fast_mode_cache
     try:
         set_fail_fast_mode(enabled)
